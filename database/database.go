fe05dd3afc4485910dffb0250f14b9d3483e4be7
Michał Krzemiński <michkrzeminski@gmail.com>
2019-02-27 18:50:12 +0100

    initial

---
+++ database/database.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"log"
+)
+
+var Database *gorm.DB
+
+func InitializeDatabase() {
+	db, err := gorm.Open(
+		"postgres",
+		"host=localhost port=5432 user=golang password=golang dbname=gwynder_schedules sslmode=disable",
+	)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("Connected to database")
+		Database = db
+	}
+}
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"gwynder-schedules/database"
+	"net/http"
+)
+
+func main() {
+	database.InitializeDatabase()
+
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
