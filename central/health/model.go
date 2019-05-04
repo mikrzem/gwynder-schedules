9372e2013c54e80250c979fc7dd21cfce045c367
michkrzeminski <michkrzeminski@gmail.com>
2019-05-04 13:39:57 +0200

    removed old version

--- central/health/model.go
+++
@@ -1,6 +0,0 @@
-package health
-
-type HealthInfo struct {
-	Healthy     bool   `json:"healthy"`
-	StartupTime string `json:"startupTime"`
-}
--- common/router.go
+++
@@ -1,5 +0,0 @@
-package common
-
-import "github.com/labstack/echo"
-
-type GroupFactory func(path string) *echo.Group
